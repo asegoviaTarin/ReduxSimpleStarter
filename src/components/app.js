c038c7609a13411eaa2c41b18549e4484a2392df
Alberto Segovia <asegovia@finametrix.com>
2018-04-26 12:27:48 +0200

    basic redux flow with image

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import BookList from "../containers/book-list";
+import BookDetail from "../containers/book-detail"; 
+import img from '../resources/diagram.png';
 
 export default class App extends Component {
+
   render() {
     return (
-      <div>React simple starter</div>
+      
+      <div>
+        <BookList />
+        <BookDetail />
+        <img src={img} height="500" width="1200"/>
+      </div>
     );
   }
 }
---
+++ src/containers/book-list.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { selectBook } from "../actions/index";
+import { bindActionCreators } from "redux";
+
+class BookList extends Component {
+  renderList() {
+    return this.props.books.map(book => {
+      return (
+        <li
+          key={book.title}
+          onClick={() => this.bookClicked(book)}
+          className="list-group-item"
+        >
+          {book.title}
+        </li>
+      );
+    });
+  }
+
+  bookClicked(book) {
+    console.log('Libro seleccionado', book)
+    this.props.selectBook(book)
+  }
+
+  render() {
+    return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
